563c0db7c4d56d7341b947cc531d1ded46e7d543
Yuvika <yuvika140604@gmail.com>
2025-11-10 15:58:21 +0530

    Update app.py for Render deployment (dynamic PORT)

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from rec import recommend  # import function from rec.py
-
+import os
+import uvicorn
 app = FastAPI()
 
 app.add_middleware(
@@ -27,3 +28,6 @@ def recommend_api(q: Query):
     results = recommend(q.query, q.top_k)
     print("Returned results from recommend():", results)
     return {"assessments": results}
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # use Render's port
+    uvicorn.run(app, host="0.0.0.0", port=port)
